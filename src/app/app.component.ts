7eeec6743dbe8829078c5f161cef3b3d08cb8e71
Ivan <ipkusliy@gmail.com>
2025-01-09 00:28:23 +0300

    small changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { AppService } from './common/app-service/app.service';
+import { BottomNavigationComponent } from "./bottom-navigation/bottom-navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule, ToastModule],
+  imports: [RouterOutlet, ButtonModule, ToastModule, BottomNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/bottom-navigation/bottom-navigation.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+// import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-bottom-navigation',
+  imports: [
+    // MatIconModule
+  ],
+  templateUrl: './bottom-navigation.component.html',
+  styleUrl: './bottom-navigation.component.scss'
+})
+export class BottomNavigationComponent {
+
+}
